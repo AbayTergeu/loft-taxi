579d4ad5f8990f1fc8203afdade9f63de314f338
abaytergeu <abaytergeu@gmail.com>
2021-10-18 20:47:01 +0600

    init project

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import {Home} from './Home';
+import {About} from './About';
+import {Profile} from './Profile';
+import React from 'react';
+
+const PAGES = {
+  home: <Home/>,
+  about: <About/>,
+  profile: <Profile/>
+}
+
+class App extends React.Component {
+  state = {currentPage: "home"};
+  navigateTo = (page) => {
+    this.setState({currentPage: page})
+  };
+
+  render(){
+    return <>
+      <header>
+        <nav>
+          <ul>          
+            <li>
+              <button onClick={() => {
+                this.navigateTo("home")
+              }}>
+                Home
+              </button>
+            </li>
+            <li>
+              <button onClick={() => {
+                this.navigateTo("about")
+              }}>
+                About                
+              </button>
+            </li>
+            <li>
+              <button onClick={() => {
+                this.navigateTo("profile")
+              }}>
+                Profile
+              </button>
+            </li>
+          </ul>
+        </nav>  
+      </header>
+      <main>
+        <section>
+          {PAGES[this.state.currentPage]}
+        </section>
+      </main>
+    </>
+  }
+}
+
+export default App;
