d066c3548821a9180eaf566f9c24595ef2c73d99
abaytergeu <abaytergeu@gmail.com>
2021-10-20 11:50:24 +0600

    add test and update profile add log out

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import App from './App'
 import {render, fireEvent} from '@testing-library/react'
 
-jest.mock('./Home', () => ({Home: () => <div>Home component</div>}))
-jest.mock('./Map', () => ({Map: () => <div>Map component</div>}))
-jest.mock('./Profile', () => ({Profile: () => <div>Profile component</div>}))
+jest.mock("./Home", () => ({ HomeWithAuth: () => <div>Home page</div> }));
+jest.mock("./Map", () => ({ Map: () => <div>Map page</div> }));
+jest.mock("./Profile", () => ({ ProfileWithAuth: () => <div>Profile page</div> }));
 
 describe("App", () => {
     it("renders correctly", () => {
         const {container} = render(<App/>)
-        expect(container.innerHTML).toMatch("Home component")
+        expect(container.innerHTML).toMatch("Home page")
     })
 
     describe("when clicked on navigations buttons", () => {
         it("opens the corresponding page", () => {
-            const {getByText, container} = render(<App />)
+            const {getByText, container} = render(<App isLoggedIn/>)
 
             fireEvent.click(getByText('Map'))
-            expect(container.innerHTML).toMatch('Map component')
+            expect(container.innerHTML).toMatch('Map page')
 
             fireEvent.click(getByText('Profile'))
-            expect(container.innerHTML).toMatch('Profile component')
+            expect(container.innerHTML).toMatch('Profile page')
         })
     })
 })
